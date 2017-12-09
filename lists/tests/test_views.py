808f69b78de63022b61b5d58dd56f9990d154360
John Doe <daftpy@gmail.com>
2017-12-09 00:44:36 -0600

    Existing List Item Form checks for duplicate entries

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,10 +2,10 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from lists.views import home_page
-from django.template.loader import render_to_string
 from django.utils.html import escape
 from lists.models import Item, List
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from unittest import skip
 
 
 class HomePageTest(TestCase):
@@ -101,6 +101,20 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
 class NewListTest(TestCase):
 
